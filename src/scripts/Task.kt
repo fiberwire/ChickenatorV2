45f9f12f1c96a36e4ba6ebe9eef135b81bd6e8a8
Unknown <mramos90@gmail.com>
2017-01-12 20:22:33 -0500

    Updated Task

--- src/scripts/Task.kt
+++ src/scripts/Task.kt
@@ -1,5 +1,12 @@
 package scripts
 
-class Task{
+import com.sun.xml.internal.ws.api.policy.PolicyResolver
+import org.powerbot.script.ClientAccessor
+import org.powerbot.script.ClientContext
 
+abstract class Task<C : ClientContext>(ctx: C) : ClientAccessor<C>(ctx) {
+
+    abstract fun activate(): Boolean
+
+    abstract fun execute()
 }
